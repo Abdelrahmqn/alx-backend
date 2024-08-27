a8e48c53dfe8de5ca3bb4e5c4bad873dfe3c3896
Abdelrahman <boda8099@gmail.com>
2024-08-28 01:19:26 +0300

    fin

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Flask Basics
+"""
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """
+    configuration class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Gets locale
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Rendering
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
