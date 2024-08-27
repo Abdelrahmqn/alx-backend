1da0233c515cbe5aa5bdc2ee1d9cbfa98e91c841
Abdelrahman <boda8099@gmail.com>
2024-08-28 01:26:26 +0300

    \

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+"""
+Flask
+"""
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Rendering
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
